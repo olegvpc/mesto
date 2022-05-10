cbc149b83dd26bcb0382f24bebffd41adf48f907
Oleg <olegvpc@yandex.ru>
2022-05-10 12:21:28 +0300

    new Sprint 5- in beginning

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const profileEditBtn = document.querySelector(".profile__edit-btn");
 const profileEdit = document.querySelector(".popup");
 const profileEditBtnClose = document.querySelector(".popup__btn-close");
@@ -35,3 +62,6 @@ function formSubmitHandler (event) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
